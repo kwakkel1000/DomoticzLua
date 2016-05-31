8a60621c286e776f42f0eead09bc2999ca6a1dd5
Gijs Kwakkel <kwakkel1000@gmail.com>
2016-05-31 22:42:45 +0200

    getTime fix

--- script_device_media.lua
+++ script_device_media.lua
@@ -1,5 +1,15 @@
 commandArray = {}
 
+function getTime(s)
+   year = string.sub(s, 1, 4)
+   month = string.sub(s, 6, 7)
+   day = string.sub(s, 9, 10)
+   hour = string.sub(s, 12, 13)
+   minutes = string.sub(s, 15, 16)
+   seconds = string.sub(s, 18, 19)
+   return os.time{year=year, month=month, day=day, hour=hour, min=minutes, sec=seconds}
+end
+
 if (otherdevices["Film"] == "On" or otherdevices["Chromecast"] == "On") then
     if (otherdevices['S Woonkamer Media'] == 'Off') then
         commandArray['S Woonkamer Media'] = 'On'
