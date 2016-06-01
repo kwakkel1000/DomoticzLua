b3a6140aba2e89f6fa0032150c8b22e12d78e75e
Gijs Kwakkel <kwakkel1000@gmail.com>
2016-06-01 18:09:34 +0200

    debug

--- script_device_media.lua
+++ script_device_media.lua
@@ -1,6 +1,4 @@
-package.path = package.path .. ';' .. '/home/pi/domoticz/scripts/lua/?.lua'
-glibFunctions = require('glib') 
-print(glibFunctions)
+glib = (loadfile "/home/pi/domoticz/scripts/lua/glib.lua")()
 
 commandArray = {}
 
@@ -9,7 +7,7 @@ if (otherdevices["Film"] == "On" or otherdevices["Chromecast"] == "On") then
         commandArray['S Woonkamer Media'] = 'On'
     end
 else
-    timeDiff = os.difftime (os.time(), glibFunctions.getTime(otherdevices_lastupdate["Chromecast"]))
+    timeDiff = os.difftime (os.time(), glib.getTime(otherdevices_lastupdate["Chromecast"]))
     if (timeDiff > 300 ) then
         if (otherdevices['S Woonkamer Media'] ~= 'Off') then
             commandArray['S Woonkamer Media'] = 'Off'
