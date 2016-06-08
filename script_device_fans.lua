b5ac0ba5b5303184a51825d786c3b538c68d5140
Gijs Kwakkel <kwakkel1000@gmail.com>
2016-06-08 17:36:10 +0200

    fix

--- script_device_fans.lua
+++ script_device_fans.lua
@@ -27,21 +27,21 @@ function motion(Motion, Fan)
     end
 end
 
-thermostaat = tonumber(otherdevices_svalues[Thermostaat])
+thermostaatValue = tonumber(otherdevices_svalues[Thermostaat])
 
 -- WOONKAMER
 if (devicechanged['M Woonkamer'] ~= nil or devicechanged['TH Woonkamer'] ~= nil or devicechanged['Thermostaat'] ~= nil or devicechanged['Film'] ~= nil or devicechanged['Chromecast'] ~= nil) then
     temp, humidity, vaag = otherdevices_svalues['TH Woonkamer']:match("([^;]+);([^;]+);([^;]+)")
-    if ((thermostaat + 5) < tonumber(temp)) then
+    if ((thermostaatValue + 5) < tonumber(temp)) then
         if (otherdevices['S Woonkamerfan'] == 'Off') then
             commandArray['S Woonkamerfan'] = 'On'
             print('turn '..value..' on (HOT)')
         end
-    elseif (otherdevices["Film"] == "On" and otherdevices["Chromecast"] == "On" and (thermostaat + 2) > tonumber(temp)) then
+    elseif (otherdevices["Film"] == "On" and otherdevices["Chromecast"] == "On" and (thermostaatValue + 2) > tonumber(temp)) then
         if (otherdevices['S Woonkamerfan'] ~= 'Off') then
             commandArray['S Woonkamerfan'] = 'Off'
         end
-    elseif (thermostaat < tonumber(temp)) then
+    elseif (thermostaatValue < tonumber(temp)) then
         motion({'M Woonkamer'}, {'S Woonkamerfan'})
     else
         if (otherdevices['S Woonkamerfan'] ~= 'Off') then
@@ -54,12 +54,12 @@ end
 -- SLAAPKAMER
 if (devicechanged['M Slaapkamer'] ~= nil or devicechanged['TH Slaapkamer'] ~= nil or devicechanged['Thermostaat'] ~= nil) then
     temp, humidity, vaag = otherdevices_svalues['TH Slaapkamer']:match("([^;]+);([^;]+);([^;]+)")
-    if ((thermostaat + 5) < tonumber(temp)) then
+    if ((thermostaatValue + 5) < tonumber(temp)) then
         if (otherdevices['S Slaapkamerfan'] == 'Off') then
             commandArray['S Slaapkamerfan'] = 'On'
             print('turn '..value..' on (HOT)')
         end
-    elseif (thermostaat < tonumber(temp)) then
+    elseif (thermostaatValue < tonumber(temp)) then
         motion({'M Slaapkamer'}, {'S Slaapkamerfan'})
     else
         if (otherdevices['S Slaapkamerfan'] ~= 'Off') then
@@ -72,12 +72,12 @@ end
 -- BADKAMER
 --if (devicechanged['M Badkamer'] ~= nil or devicechanged['TH Badkamer'] ~= nil or devicechanged['Thermostaat'] ~= nil) then
 --    temp, humidity, vaag = otherdevices_svalues['TH Slaapkamer']:match("([^;]+);([^;]+);([^;]+)")
---    if ((thermostaat + 5) < tonumber(temp)) then
+--    if ((thermostaatValue + 5) < tonumber(temp)) then
 --        if (otherdevices['S Slaapkamerfan'] == 'Off') then
 --            commandArray['S Slaapkamerfan'] = 'On'
 --            print('turn '..value..' on (HOT)')
 --        end
---    elseif (thermostaat < tonumber(temp)) then
+--    elseif (thermostaatValue < tonumber(temp)) then
 --        motion({'M Slaapkamer'}, {'S Slaapkamerfan'})
 --    else
 --        if (otherdevices['S Slaapkamerfan'] ~= 'Off') then
