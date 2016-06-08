ca77d45cf49f8f8eeab3653b4fbf3c05f067ce8c
Gijs Kwakkel <kwakkel1000@gmail.com>
2016-06-09 00:18:07 +0200

    use functions

--- script_device_media.lua
+++ script_device_media.lua
@@ -4,15 +4,11 @@ glib = require('glib')
 commandArray = {}
 
 if (otherdevices["Film"] == "On" or otherdevices["Chromecast"] == "On") then
-    if (otherdevices['S Woonkamer Media'] == 'Off') then
-        commandArray['S Woonkamer Media'] = 'On'
-    end
+    glib.turnOn('S Woonkamer Media')
 else
     timeDiff = os.difftime (os.time(), glib.getTime(otherdevices_lastupdate["Chromecast"]))
     if (timeDiff > 300 ) then
-        if (otherdevices['S Woonkamer Media'] ~= 'Off') then
-            commandArray['S Woonkamer Media'] = 'Off'
-        end
+        glib.turnOff('S Woonkamer Media')
     end
 end
 
