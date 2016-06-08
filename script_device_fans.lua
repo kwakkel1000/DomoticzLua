6fcce17c6732d22aa2a8d3310d9fbedd659923e7
Gijs Kwakkel <kwakkel1000@gmail.com>
2016-06-08 23:12:19 +0200

    fixed fuckup

--- script_device_fans.lua
+++ script_device_fans.lua
@@ -79,8 +79,8 @@ end
 
 -- BADKAMER
 if ((hour == 6 and minute >= 30) or (hour == 9) or (hour == 8 and minute <= 59)) then
-    if (otherdevices['S Woonkamer Media'] == 'Off') then
-        commandArray['S Woonkamer Media'] = 'On'
+    if (otherdevices['S Badkamerfan'] == 'Off') then
+        commandArray['S Badkamerfan'] = 'On'
     end
 else
 --    if (devicechanged['M Badkamer'] ~= nil or devicechanged['TH Badkamer'] ~= nil or devicechanged['Thermostaat'] ~= nil) then
@@ -88,8 +88,8 @@ else
 --        temp = glib.getAverage({tonumber(woonkamerTemp), tonumber(eetkamerTemp)})
 --        setFan(temp, {'S Badkamerfan'}, {'M Badkamer'}, false)
 --    end
-    if (otherdevices['S Woonkamer Media'] ~= 'Off') then -- temporary
---        commandArray['S Woonkamer Media'] = 'Off' -- temporary
+    if (otherdevices['S Badkamerfan'] ~= 'Off') then -- temporary
+        commandArray['S Badkamerfan'] = 'Off' -- temporary
     end -- temporary
 end
 
