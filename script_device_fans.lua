1df36e50f3071789fbaf8b53ce1ef637a71c4349
Gijs Kwakkel <kwakkel1000@gmail.com>
2016-06-09 19:07:50 +0200

    fix

--- script_device_fans.lua
+++ script_device_fans.lua
@@ -5,9 +5,8 @@ hotOffset = 10
 movieOffset = 5
 
 
-currentTime = os.time()
-hour = string.sub(currentTime, 12, 13)
-minute = string.sub(currentTime, 15, 16)
+currentHour = os.date("%H")
+currentMinute = os.date("%M")
 
 commandArray = {}
 
@@ -66,7 +65,7 @@ if (devicechanged['M Slaapkamer'] ~= nil or devicechanged['TH Slaapkamer'] ~= ni
 end
 
 -- BADKAMER
-if (not glib.timerOn(6,30 , 9,30 , tonumber(hour),tonumber(minute) ,{'S Badkamerfan'}) and not glib.timerOn(22,30 , 23,45 , tonumber(hour),tonumber(minute) ,{'S Badkamerfan'})) then
+if (not glib.timerOn(6,30 , 9,30 , currentHour,currentMinute ,{'S Badkamerfan'}) and not glib.timerOn(22,30 , 23,45 , currentHour,currentMinute ,{'S Badkamerfan'})) then
 --    if (devicechanged['M Badkamer'] ~= nil or devicechanged['TH Badkamer'] ~= nil or devicechanged['Thermostaat'] ~= nil) then
 --        badkamerTemp, badkamerHumidity, badkamerVaag = otherdevices_svalues['TH Badkamer']:match("([^;]+);([^;]+);([^;]+)")
 --        temp = glib.getAverage({tonumber(woonkamerTemp), tonumber(eetkamerTemp)})
