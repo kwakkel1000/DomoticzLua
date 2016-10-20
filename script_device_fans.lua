08e8d7e5ef72046622108f77db59c7f0ebdf66a0
Gijs Kwakkel <kwakkel1000@gmail.com>
2016-10-20 22:19:22 +0200

    added hold

--- script_device_fans.lua
+++ script_device_fans.lua
@@ -58,33 +58,36 @@ function setFan(temp, fans, motions, movie)
     end
 end
 
--- WOONKAMER
-if (devicechanged['M Woonkamer'] ~= nil or devicechanged['M Eetkamer'] ~= nil or devicechanged['TH Woonkamer'] ~= nil or devicechanged['TH Eetkamer'] ~= nil or devicechanged['Thermostaat'] ~= nil or devicechanged['Film'] ~= nil or devicechanged['Chromecast'] ~= nil or devicechanged['Air'] ~= nil) then
-    woonkamerTemp, woonkamerHumidity, woonkamerVaag = otherdevices_svalues['TH Woonkamer']:match("([^;]+);([^;]+);([^;]+)")
-    eetkamerTemp, eetkamerHumidity, eetkamerVaag = otherdevices_svalues['TH Eetkamer']:match("([^;]+);([^;]+);([^;]+)")
-    temp = glib.getAverage({tonumber(woonkamerTemp), tonumber(eetkamerTemp)})
-    setFan(temp, {'S Woonkamerfan'}, {'M Woonkamer', 'M Eetkamer'}, glib.moviePlaying('Woonkamer'))
-end
 
-
--- SLAAPKAMER
-if (devicechanged['M Slaapkamer'] ~= nil or devicechanged['TH Slaapkamer'] ~= nil or devicechanged['Thermostaat'] ~= nil or devicechanged['Air'] ~= nil) then
-    slaapkamerTemp, slaapkamerHumidity, slaapkamerVaag = otherdevices_svalues['TH Slaapkamer']:match("([^;]+);([^;]+);([^;]+)")
-    temp = glib.getAverage({tonumber(slaapkamerTemp)})
-    setFan(temp, {'S Slaapkamerfan'}, {'M Slaapkamer'}, false)
-end
-
--- BADKAMER
---if (devicechanged['M Badkamer'] ~= nil or devicechanged['TH Badkamer'] ~= nil or devicechanged['Thermostaat'] ~= nil or devicechanged['Air'] ~= nil) then
-    if (otherdevices['Air'] == "On") then
-        glib.turnOn('S Badkamerfan')
-    elseif (not (glib.timerOn(6,30 , 9,30 , currentHour,currentMinute ,{'S Badkamerfan'}) or glib.timerOn(13,30 , 15,30 , currentHour,currentMinute ,{'S Badkamerfan'}) or glib.timerOn(23,00 , 23,59 , currentHour,currentMinute ,{'S Badkamerfan'}))) then
---        badkamerTemp, badkamerHumidity, badkamerVaag = otherdevices_svalues['TH Badkamer']:match("([^;]+);([^;]+);([^;]+)")
---        temp = glib.getAverage({tonumber(woonkamerTemp), tonumber(eetkamerTemp)})
---        setFan(temp, {'S Badkamerfan'}, {'M Badkamer'}, false)
-        glib.turnOff('S Badkamerfan') -- temporary
+if (otherdevices['Hold'] == "Off") then
+    -- WOONKAMER
+    if (devicechanged['M Woonkamer'] ~= nil or devicechanged['M Eetkamer'] ~= nil or devicechanged['TH Woonkamer'] ~= nil or devicechanged['TH Eetkamer'] ~= nil or devicechanged['Thermostaat'] ~= nil or devicechanged['Film'] ~= nil or devicechanged['Chromecast'] ~= nil or devicechanged['Air'] ~= nil) then
+        woonkamerTemp, woonkamerHumidity, woonkamerVaag = otherdevices_svalues['TH Woonkamer']:match("([^;]+);([^;]+);([^;]+)")
+        eetkamerTemp, eetkamerHumidity, eetkamerVaag = otherdevices_svalues['TH Eetkamer']:match("([^;]+);([^;]+);([^;]+)")
+        temp = glib.getAverage({tonumber(woonkamerTemp), tonumber(eetkamerTemp)})
+        setFan(temp, {'S Woonkamerfan'}, {'M Woonkamer', 'M Eetkamer'}, glib.moviePlaying('Woonkamer'))
     end
---end
+    
+    
+    -- SLAAPKAMER
+    if (devicechanged['M Slaapkamer'] ~= nil or devicechanged['TH Slaapkamer'] ~= nil or devicechanged['Thermostaat'] ~= nil or devicechanged['Air'] ~= nil) then
+        slaapkamerTemp, slaapkamerHumidity, slaapkamerVaag = otherdevices_svalues['TH Slaapkamer']:match("([^;]+);([^;]+);([^;]+)")
+        temp = glib.getAverage({tonumber(slaapkamerTemp)})
+        setFan(temp, {'S Slaapkamerfan'}, {'M Slaapkamer'}, false)
+    end
+    
+    -- BADKAMER
+    --if (devicechanged['M Badkamer'] ~= nil or devicechanged['TH Badkamer'] ~= nil or devicechanged['Thermostaat'] ~= nil or devicechanged['Air'] ~= nil) then
+        if (otherdevices['Air'] == "On") then
+            glib.turnOn('S Badkamerfan')
+        elseif (not (glib.timerOn(6,30 , 9,30 , currentHour,currentMinute ,{'S Badkamerfan'}) or glib.timerOn(13,30 , 15,30 , currentHour,currentMinute ,{'S Badkamerfan'}) or glib.timerOn(23,00 , 23,59 , currentHour,currentMinute ,{'S Badkamerfan'}))) then
+    --        badkamerTemp, badkamerHumidity, badkamerVaag = otherdevices_svalues['TH Badkamer']:match("([^;]+);([^;]+);([^;]+)")
+    --        temp = glib.getAverage({tonumber(woonkamerTemp), tonumber(eetkamerTemp)})
+    --        setFan(temp, {'S Badkamerfan'}, {'M Badkamer'}, false)
+            glib.turnOff('S Badkamerfan') -- temporary
+        end
+    --end
+end
 
 return commandArray
 
--- script_time_thermostat.lua
+++ script_time_thermostat.lua
@@ -3,6 +3,7 @@ glib = require('glib')
 
 awayTemp = "15.00"
 bedroomTemp = "18.00"
+wakeupTemp = "19.00"
 normalTemp = "20.00"
 tvTemp = "21.00"
 wakeupStartHour = 7
@@ -22,32 +23,34 @@ minutes = currentHour * 60 + currentMinute
 
 commandArray = {}
 
-timeDiff = os.difftime (os.time(), glib.getTime(otherdevices_lastupdate[thermostaatName]))
-if (timeDiff > 1800 ) then -- only change thermostat if it hasn't changed for 30minutes
-    motionDetected = false
-    for key, value in pairs({'M Woonkamer', 'M Eetkamer'}) do
-        if (tonumber(otherdevices_svalues[value]) > 1) then
-            motionDetected = true
+if (otherdevices['Hold'] == "Off") then
+    timeDiff = os.difftime (os.time(), glib.getTime(otherdevices_lastupdate[thermostaatName]))
+    if (timeDiff > 1200) then -- only change thermostat if it hasn't changed for 20minutes
+        motionDetected = false
+        for key, value in pairs({'M Woonkamer', 'M Eetkamer'}) do
+            if (tonumber(otherdevices_svalues[value]) > 1) then
+                motionDetected = true
+            end
         end
-    end
-    if (otherdevices["Film"] == "On" or otherdevices["Chromecast"] == "On" or otherdevices["Game"] == "On") then
-        temp = tvTemp
-        print('tv, set temp for '..thermostaatName..' to '..tostring(temp))
-    elseif (motionDetected == true) then
-        temp = normalTemp
-        print('set temp for '..thermostaatName..' to '..tostring(temp))
-    elseif (wakeupStart <= minutes and wakeupEnd >= minutes) then
-        temp = normalTemp
-        print('waking up, set temp for '..thermostaatName..' to '..tostring(temp))
-    else
-        temp = awayTemp
-        print('away, set temp for '..thermostaatName..' to '..tostring(temp))
-    end
-    thermostaatValue = otherdevices_svalues[thermostaatName]
-    if (thermostaatValue ~= temp) then
-        set = tostring(thermostaatIdx) .. "|0|" .. temp
-        commandArray['UpdateDevice'] = set
-        print 'actually setting the temperature'
+        if (otherdevices["Film"] == "On" or otherdevices["Chromecast"] == "On" or otherdevices["Game"] == "On") then
+            temp = tvTemp
+            print('tv, set temp for '..thermostaatName..' to '..tostring(temp))
+        elseif (motionDetected == true) then
+            temp = normalTemp
+            print('set temp for '..thermostaatName..' to '..tostring(temp))
+        elseif (wakeupStart <= minutes and wakeupEnd >= minutes) then
+            temp = wakeupTemp
+            print('waking up, set temp for '..thermostaatName..' to '..tostring(temp))
+        else
+            temp = awayTemp
+            print('away, set temp for '..thermostaatName..' to '..tostring(temp))
+        end
+        thermostaatValue = otherdevices_svalues[thermostaatName]
+        if (thermostaatValue ~= temp) then
+            set = tostring(thermostaatIdx) .. "|0|" .. temp
+            commandArray['UpdateDevice'] = set
+            print 'actually setting the temperature'
+        end
     end
 end
 
